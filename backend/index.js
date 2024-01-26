37ecd71c3435d45c54f7f9fc5dd2f00dea50b6bd
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-01-27 01:39:03 +0530

    environment variable set and first request test

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
-import express from 'express';
-import { PORT } from './config.js';
+import express from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Hello World");
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
