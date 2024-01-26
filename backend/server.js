8b463eea49371528a70bfe6e20e731cc2dc640f0
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-01-27 03:23:39 +0530

    CRUD operations done

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { Book } from "./models/book_model.js";
+
 const app = express();
+
+// Middleware for parsing request bodies
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 const URL = process.env.MONGO_DB_URL;
@@ -20,9 +25,76 @@ const connection = () => {
     });
 };
 
-app.get("/", (req, res) => {
-  console.log(req);
-  return res.status(234).send("Hello World");
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publisherYear) {
+      return res.status(400).send({ message: "Send all required fields..!" });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publisherYear: req.body.publisherYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(200).json(book);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    return res.status(200).json({ count: books.length, data: books });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const books = await Book.findById(req.params.id);
+    return res.status(200).json(books);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publisherYear) {
+      return res.status(400).send({ message: "Send all required fields..!" });
+    }
+    const result = await Book.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!result) {
+      return res.status(404).send({ message: "Book not found..!" });
+    }
+
+    return res.status(200).json({ message: "Book updated successfully..!" });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const result = await Book.findByIdAndDelete(req.params.id);
+    if (!result) {
+      return res.status(404).send({ message: "Book not found..!" });
+    }
+    return res.status(200).send({ message: "Book deleted successfully..!" });
+    
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).send({ message: e.message });
+  }
 });
 
 app.listen(PORT, () => {
