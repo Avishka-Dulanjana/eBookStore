1733afa87bd8c41b2b854efaa5b09673319794ab
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-01-30 02:39:26 +0530

    CORS policy set

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,19 @@ const app = express();
 // Middleware for parsing request bodies
 app.use(express.json());
 
+// Middleware for handling CORS policy
+// -- 01. Allow all requests from all origins with default CORS(*)
+app.use(cors());
+
+// -- 02. Allow specific origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 const URL = process.env.MONGO_DB_URL;
 
